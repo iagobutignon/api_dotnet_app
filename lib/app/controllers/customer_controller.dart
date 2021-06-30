6f271b68afdf2c5790873c5b6f92ea1c23085332
Iago Butignon <iagobutignon.fatec@gmail.com>
2021-06-30 17:08:18 -0300

    Filtro implementado

--- lib/app/controllers/customer_controller.dart
+++ lib/app/controllers/customer_controller.dart
@@ -6,11 +6,86 @@ class CustomerController with ChangeNotifier {
   List<CustomerModel> _customers = [];
   CustomerModel _customerEditing = CustomerModel();
   bool _newCustomer = true;
+  String _filter = '';
+  String _filterOption = 'name';
 
-  List<CustomerModel> get customers => [..._customers];
+  List<CustomerModel> get customers {
+    if (_filter == '' || _filter.isEmpty) {
+      return [..._customers];
+    }
+    if (_filterOption == 'name') {
+      return [..._customers]
+          .where((customer) =>
+              customer.name.toLowerCase().contains(_filter.toLowerCase()))
+          .toList();
+    } else if (_filterOption == 'cpfCnpj') {
+      return [..._customers]
+          .where((customer) => customer.cpfCnpj == _filter)
+          .toList();
+    } else if (_filterOption == 'status') {
+      if (_filter == 'active') {
+        return [..._customers]
+            .where((customer) => customer.active == true)
+            .toList();
+      }
+      if (_filter == 'inactive') {
+        return [..._customers]
+            .where((customer) => customer.active == false)
+            .toList();
+      }
+    }
+    return [..._customers];
+  }
+
+  int get count {
+    if (_filter == '' || _filter.isEmpty) {
+      return _customers.length;
+    }
+    if (_filterOption == 'name') {
+      return [..._customers]
+          .where((customer) =>
+              customer.name.toLowerCase().contains(_filter.toLowerCase()))
+          .toList()
+          .length;
+    } else if (_filterOption == 'cpfCnpj') {
+      return [..._customers]
+          .where((customer) => customer.cpfCnpj == _filter)
+          .toList()
+          .length;
+    } else if (_filterOption == 'status') {
+      if (_filter == 'active') {
+        return [..._customers]
+            .where((customer) => customer.active == true)
+            .toList()
+            .length;
+      }
+      if (_filter == 'inactive') {
+        return [..._customers]
+            .where((customer) => customer.active == false)
+            .toList()
+            .length;
+      }
+    }
+    return _customers.length;
+  }
+
   CustomerModel get customerEditing => _customerEditing;
-  int get count => _customers.length;
   bool get newCustomer => _newCustomer;
+  String get filter => _filter;
+  String get filterOption => _filterOption;
+
+  set filter(String filter) {
+    _filter = filter;
+
+    notifyListeners();
+  }
+
+  set filterOption(String option) {
+    _filter = '';
+    _filterOption = option;
+
+    notifyListeners();
+  }
 
   set customers(List<CustomerModel> customers) {
     _customers = customers;
--- lib/app/views/customers/customers_view.dart
+++ lib/app/views/customers/customers_view.dart
@@ -34,24 +34,21 @@ class _CustomersViewState extends State<CustomersView> {
         Provider.of<CustomerController>(context, listen: true);
 
     return ListView.builder(
-      itemCount: controller.count,
+      itemCount: controller.count + 1,
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(controller.customers[index].name),
-          subtitle: Text(
-              'Endereços: ${controller.customers[index].addresses.length}'),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              IconButton(
-                onPressed: () => _edit(controller.customers[index]),
-                icon: Icon(Icons.edit),
-              ),
-              IconButton(
-                onPressed: () => _remove(controller.customers[index]),
-                icon: Icon(Icons.delete),
-              ),
-            ],
+        if (index == 0) return SizedBox(height: 70);
+        return Card(
+          child: ListTile(
+            title: Text('${controller.customers[index - 1].name}'),
+            subtitle: Text('${controller.customers[index - 1].cpfCnpj}'),
+            leading: CircleAvatar(
+              child: Icon(Icons.person, size: 30),
+            ),
+            trailing: IconButton(
+              onPressed: () => _remove(controller.customers[index - 1]),
+              icon: Icon(Icons.delete, size: 30),
+            ),
+            onTap: () => _edit(controller.customers[index - 1]),
           ),
         );
       },
@@ -74,13 +71,218 @@ class _CustomersViewState extends State<CustomersView> {
             onPressed: _refresh,
             icon: Icon(Icons.refresh),
           ),
-          IconButton(
-            onPressed: _add,
-            icon: Icon(Icons.add),
+        ],
+      ),
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          _customerList(),
+          SearchInput(
+            label: 'Filtrar',
           ),
         ],
       ),
-      body: _customerList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _add,
+        child: Icon(
+          Icons.add,
+          size: 36,
+        ),
+      ),
+    );
+  }
+}
+
+class SearchInput extends StatefulWidget {
+  final String label;
+
+  SearchInput({
+    Key? key,
+    required this.label,
+  }) : super(key: key);
+
+  @override
+  _SearchInputState createState() => _SearchInputState();
+}
+
+class _SearchInputState extends State<SearchInput> {
+  IconData icon = Icons.person;
+  var filterStatus = 'active';
+  TextEditingController _filterController = TextEditingController();
+
+  Future<void> _filter(String filter) async {
+    Provider.of<CustomerController>(context, listen: false).filter = filter;
+  }
+
+  @override
+  void initState() {
+    icon = Icons.person;
+    filterStatus = 'active';
+    super.initState();
+  }
+
+  Future<void> _selectStatus() async {
+    filterStatus = await showMenu(
+      items: <PopupMenuEntry>[
+        PopupMenuItem(
+          value: 'all',
+          child: Row(
+            children: <Widget>[
+              Icon(
+                Icons.list,
+                color: Colors.blue,
+              ),
+              Text(" Todos"),
+            ],
+          ),
+        ),
+        PopupMenuItem(
+          value: 'active',
+          child: Row(
+            children: <Widget>[
+              Icon(
+                Icons.work_outlined,
+                color: Colors.blue,
+              ),
+              Text(" Ativos"),
+            ],
+          ),
+        ),
+        PopupMenuItem(
+          value: 'inactive',
+          child: Row(
+            children: <Widget>[
+              Icon(
+                Icons.work_off_outlined,
+                color: Colors.blue,
+              ),
+              Text(" Inativos"),
+            ],
+          ),
+        ),
+      ],
+      context: context,
+      position: RelativeRect.fromLTRB(100, 100, 0, 0),
+    );
+
+    if (filterStatus == 'active') {
+      _filterController.text = 'Ativos';
+    } else if (filterStatus == 'inactive') {
+      _filterController.text = 'Inativos';
+    } else {
+      _filterController.text = 'Todos';
+    }
+    _filter(filterStatus);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: Container(
+        height: 50,
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(2),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(2),
+          child: Row(
+            children: [
+              InkWell(
+                onTap: () async {
+                  final filter = await showMenu(
+                    items: <PopupMenuEntry>[
+                      PopupMenuItem(
+                        value: 'name',
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.person,
+                              color: Colors.blue,
+                            ),
+                            Text(" Nome"),
+                          ],
+                        ),
+                      ),
+                      PopupMenuItem(
+                        value: 'cpfCnpj',
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.credit_card,
+                              color: Colors.blue,
+                            ),
+                            Text(" CPF / CNPJ"),
+                          ],
+                        ),
+                      ),
+                      PopupMenuItem(
+                        value: 'status',
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.work_off_outlined,
+                              color: Colors.blue,
+                            ),
+                            Text(" Status"),
+                          ],
+                        ),
+                      ),
+                    ],
+                    context: context,
+                    position: RelativeRect.fromLTRB(0, 100, 0, 0),
+                  );
+                  if (filter == 'name') {
+                    setState(() {
+                      icon = Icons.person;
+                    });
+                  }
+                  if (filter == 'cpfCnpj') {
+                    setState(() {
+                      icon = Icons.credit_card;
+                    });
+                  }
+                  if (filter == 'status') {
+                    setState(() {
+                      icon = Icons.work_off_outlined;
+                    });
+                  }
+                  _filterController.text = '';
+                  Provider.of<CustomerController>(context, listen: false)
+                      .filterOption = filter;
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Icon(icon, color: Colors.blue),
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  onChanged: (v) => _filter(v),
+                  onTap: Provider.of<CustomerController>(context, listen: false)
+                              .filterOption ==
+                          'status'
+                      ? () => _selectStatus()
+                      : null,
+                  readOnly:
+                      Provider.of<CustomerController>(context, listen: false)
+                                  .filterOption ==
+                              'status'
+                          ? true
+                          : false,
+                  controller: _filterController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: widget.label,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
